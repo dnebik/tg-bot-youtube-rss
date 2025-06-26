d83e32b955a9af1259e01e0c8e61de9613601c5c
dnebik <ya.dneb@yandex.ru>
2025-06-27 02:48:10 +0300

    update HTTP service to support SOCKS5 proxy; adjust Docker configurations

--- src/service/http.ts
+++ src/service/http.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { SocksProxyAgent } from "socks-proxy-agent";
 
 export const http = axios.create();
 
@@ -6,9 +7,9 @@ const APP_PROXY_HOST = process.env.APP_PROXY_HOST;
 const APP_PROXY_PORT = process.env.APP_PROXY_PORT;
 
 if (APP_PROXY_HOST && APP_PROXY_PORT) {
-  http.defaults.proxy = {
-    host: APP_PROXY_HOST,
-    port: Number(APP_PROXY_PORT),
-    protocol: "http",
-  };
+  const agent = new SocksProxyAgent(
+    `socks5h://${APP_PROXY_HOST}:${APP_PROXY_PORT}`,
+  );
+  http.defaults.httpsAgent = agent;
+  http.defaults.httpAgent = agent;
 }
